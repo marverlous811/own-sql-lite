6f0eec19adcd274cd7c6ad2db210c53721273083
hieunq <hieunq@ows.vn>
2020-02-26 11:06:11 +0700

    add cursor

--- main.cpp
+++ main.cpp
@@ -129,9 +129,41 @@ typedef struct {
     Pager* pager;
 } Table;
 
-void* row_slot(Table* table, uint32_t row_num){
+typedef struct {
+    Table* table;
+    uint32_t row_num;
+    bool end_of_table;
+} Cursor;
+
+Cursor* table_start(Table* table){
+    Cursor* cursor = (Cursor*)malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = (table->num_rows == 0);
+
+    return cursor;
+}
+
+Cursor* table_end(Table* table){
+    Cursor* cursor = (Cursor*)malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+
+    return cursor;
+}
+
+void cursor_advance(Cursor* cursor){
+    cursor->row_num += 1;
+    if(cursor->row_num >= cursor->table->num_rows){
+        cursor->end_of_table = true;
+    }
+}
+
+void* cursor_value(Cursor* cursor){
+    uint32_t row_num = cursor->row_num;
     uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void* page = get_page(table->pager, page_num);
+    void* page = get_page(cursor->table->pager, page_num);
     uint32_t row_offset = row_num % ROWS_PER_PAGE;
     uint32_t byte_offset = row_offset * USER_ROW_SIZE;
     return (char*)page + byte_offset;
@@ -314,19 +346,26 @@ ExecResult exec_insert(Statement* statement, Table* table) {
     }
 
     UserRow* row_to_insert = &(statement->row_to_insert);
+    Cursor* cursor = table_end(table);
 
-    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    serialize_row(row_to_insert, cursor_value(cursor));
     table->num_rows += 1;
+
+    free(cursor);
 
     return EXECUTE_SUCCESS;
 }
 
 ExecResult exec_select(Statement* statement, Table* table){
     UserRow row;
-    for(uint32_t i = 0; i < table->num_rows; i++){
-        deserialize_row(row_slot(table, i), &row);
+    Cursor* cursor = table_start(table);
+    while(!(cursor->end_of_table)){
+        deserialize_row(cursor_value(cursor), &row);
         print_row(&row);
+        cursor_advance(cursor);
     }
+
+    free(cursor);
 
     return EXECUTE_SUCCESS;
 }
