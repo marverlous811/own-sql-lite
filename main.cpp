489562e9b8ee36533933b190fb056ffcfdc9037f
hieunq <hieunq@ows.vn>
2020-02-26 10:27:54 +0700

    save data to file

--- main.cpp
+++ main.cpp
@@ -2,11 +2,93 @@
 #include "stdlib.h"
 #include "stdarg.h"
 #include "string.h"
+#include "errno.h"
+#include "fcntl.h"
+#include "unistd.h"
+
+__attribute__ ((cold))
+__attribute__ ((noreturn))
+__attribute__ ((format (printf, 1, 2)))
+static void panic(const char *format, ...) {
+    va_list ap;
+    va_start(ap, format);
+    vfprintf(stderr, format, ap);
+    fprintf(stderr, "\n");
+    va_end(ap);
+    abort();
+}
 
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 #define TABLE_MAX_PAGES 100
 
 const uint32_t PAGE_SIZE = 4096;
+
+typedef struct {
+    int fd;
+    uint32_t file_len;
+    void* pages[TABLE_MAX_PAGES];
+} Pager;
+
+Pager* pager_open(const char* filename){
+    int fd = open(filename, O_RDWR | O_CREAT, S_IWUSR | S_IRUSR );
+    if(fd == -1) {
+        panic("Unable to open file\n");
+    }
+
+    off_t file_len = lseek(fd, 0, SEEK_END);
+
+    Pager* pager = (Pager*)malloc(sizeof(Pager));
+    pager->fd = fd;
+    pager->file_len = file_len;
+
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++){
+        pager->pages[i] = NULL;
+    }
+
+    return pager;
+}
+
+void pager_flush(Pager* pager, uint32_t page_num, uint32_t size) {
+    if(pager->pages[page_num] == NULL) {
+        panic("Tried to flush null page\n");
+    }
+
+    off_t offset = lseek(pager->fd, page_num * PAGE_SIZE, SEEK_SET);
+    if(offset == -1){
+        panic("Error seeking: %d\n", errno);
+    }
+
+    ssize_t bytes_written = write(pager->fd, pager->pages[page_num], size);
+    if(bytes_written == -1) panic("Error writing: %d\n", errno);
+}
+
+void* get_page(Pager* pager, uint32_t page_num){
+    if(page_num > TABLE_MAX_PAGES) {
+        panic("Tried to fetch page number out of bounds %d > %d\n", page_num, TABLE_MAX_PAGES);
+    }
+
+    if(pager->pages[page_num] == NULL){
+        //Cache miss. Allocate memory and load from file
+        void* page = malloc(PAGE_SIZE);
+        uint32_t num_pages = pager->file_len / PAGE_SIZE;
+
+        if(pager->file_len % PAGE_SIZE){
+            num_pages += 1;
+        }
+
+        if(page_num <= num_pages){
+            lseek(pager->fd, page_num * PAGE_SIZE, SEEK_SET);
+            ssize_t bytes_read = read(pager->fd, page, PAGE_SIZE);
+            if (bytes_read == -1) {
+                panic("Error reading file: %d\n", errno);
+            }
+        }
+
+        pager->pages[page_num] = page;
+    }
+
+    return pager->pages[page_num];
+}
 
 //**************Fixed table*************************//
 
@@ -44,17 +126,12 @@ const uint32_t TABLE_MAX_ROW = ROWS_PER_PAGE * TABLE_MAX_PAGES;
 
 typedef struct {
     uint32_t num_rows;
-    void* pages[TABLE_MAX_PAGES];
+    Pager* pager;
 } Table;
 
 void* row_slot(Table* table, uint32_t row_num){
     uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void* page = table->pages[page_num];
-    if(page == NULL) {
-        //Allocate memory only when try to access page
-        page = table->pages[page_num] = malloc(PAGE_SIZE);
-    }
-
+    void* page = get_page(table->pager, page_num);
     uint32_t row_offset = row_num % ROWS_PER_PAGE;
     uint32_t byte_offset = row_offset * USER_ROW_SIZE;
     return (char*)page + byte_offset;
@@ -64,37 +141,58 @@ void print_row(UserRow* row){
     printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
 
-Table* new_table(){
+Table* db_open(const char* filename){
+    Pager* pager = pager_open(filename);
+    uint32_t num_rows = pager->file_len / USER_ROW_SIZE;
+
     Table* table = (Table*)malloc(sizeof(Table));
-    table->num_rows = 0;
-    for(uint32_t i = 0; i < TABLE_MAX_PAGES; i++){
-        table->pages[i] = NULL;
-    }
+    table->pager = pager;
+    table->num_rows = num_rows;
 
     return table;
 }
 
-void free_table(Table* table){
-    for(int i = 0; table->pages[i]; i++){
-        free(table->pages[i]);
+void db_close(Table* table) {
+    Pager* pager = table->pager;
+    uint32_t num_full_pagers = table->num_rows / ROWS_PER_PAGE;
+
+    for(uint32_t i = 0; i < num_full_pagers; i++){
+        if(pager->pages[i] == NULL) {
+            continue;
+        }
+
+        pager_flush(pager, i, PAGE_SIZE);
+        free(pager->pages[i]);
+        pager->pages[i] = NULL;
     }
 
+    uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
+    if(num_additional_rows > 0 ) {
+        uint32_t page_num = num_full_pagers;
+        if(pager->pages[page_num] != NULL){
+            pager_flush(pager, page_num, num_additional_rows * USER_ROW_SIZE);
+            free(pager->pages[page_num]);
+            pager->pages[page_num] = NULL;
+        }
+    }
+
+    int result = close(pager->fd);
+    if(result == -1) panic("Error closing db file.\n");
+
+    for(uint32_t i = 0; i < TABLE_MAX_PAGES; i++){
+        void* page = pager->pages[i];
+        if(page){
+            free(page);
+            pager->pages[i] = NULL;
+        }
+    }
+
+    free(pager);
     free(table);
 }
 
-//==================================================//
 
-__attribute__ ((cold))
-__attribute__ ((noreturn))
-__attribute__ ((format (printf, 1, 2)))
-static void panic(const char *format, ...) {
-    va_list ap;
-    va_start(ap, format);
-    vfprintf(stderr, format, ap);
-    fprintf(stderr, "\n");
-    va_end(ap);
-    abort();
-}
+//==================================================//
 
 typedef enum {
     META_COMMAND_SUCCESS,
@@ -159,8 +257,9 @@ void close_input_buffer(InputBuffer* ibuffer){
     free(ibuffer);
 }
 
-MetaCommandResult exec_meta_command(InputBuffer *ibuffer){
+MetaCommandResult exec_meta_command(InputBuffer *ibuffer, Table* table){
     if(strcmp(ibuffer->buffer, ".exit") == 0) {
+        db_close(table);
         exit(EXIT_SUCCESS);
     } else  {
         return META_COMMAND_UNRECOGNIZED_COMMAND;
@@ -242,15 +341,19 @@ ExecResult exec_statement(Statement* statement,  Table* table){
     }
 }
 
-int main(){
+int main(int argc, char* argv[]){
+    if(argc < 2) panic("Must supply a db filename.\n");
+
+    char* filename = argv[1];
+
     InputBuffer* ibuffer = new_input_buffer();
-    Table* table = new_table();
+    Table* table = db_open(filename);
     while(true){
         print_promt();
         read_input(ibuffer);
 
         if(ibuffer->buffer[0] == '.'){
-            switch (exec_meta_command(ibuffer))
+            switch (exec_meta_command(ibuffer, table))
             {
             case META_COMMAND_SUCCESS:
                 break;
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,7 +1,10 @@
 describe 'database' do 
+    before do
+        `rm -rf ./bin/test.db`
+    end
     def run_script(commands)
         raw_output = nil
-        IO.popen("./bin/db", "r+") do |pipe|
+        IO.popen("./bin/db ./bin/test.db", "r+") do |pipe|
             commands.each do |command|
                 pipe.puts command
             end
@@ -72,15 +75,35 @@ describe 'database' do
 
     it 'prints an error message if id is negative' do
         script = [
-          "insert -1 cstack foo@bar.com",
-          "select",
-          ".exit",
+            "insert -1 cstack foo@bar.com",
+            "select",
+            ".exit",
         ]
         result = run_script(script)
         expect(result).to match_array([
-          "db > ID must be positive.",
-          "db > Executed.",
-          "db > ",
+            "db > ID must be positive.",
+            "db > Executed.",
+            "db > ",
+        ])
+    end
+
+    it 'keeps data after closing connection' do
+        result1 = run_script([
+            "insert 1 user1 person1@example.com",
+            ".exit",
+        ])
+        expect(result1).to match_array([
+            "db > Executed.",
+            "db > ",
+        ])
+        result2 = run_script([
+            "select",
+            ".exit",
+        ])
+        expect(result2).to match_array([
+            "db > (1, user1, person1@example.com)",
+            "Executed.",
+            "db > ",
         ])
     end
 end
